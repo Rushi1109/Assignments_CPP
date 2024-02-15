1662143582f8a49eab59d392130ed8180d74a199
Rushi Gandhi <rushigandhi1109@gmail.com>
2024-02-15 18:55:06 +0530

    Refactor LinkedList class to have interface and implementation separate

--- LinkedList/include/LinkedList.h
+++ LinkedList/include/LinkedList.h
@@ -32,130 +32,16 @@ public:
 		}
 	}
 
+	unsigned int length() const;
+	void display() const;
 
-	unsigned int length() const {
-		if (!head) {
-			return 0;
-		}
+	void insertAtHead(T val);
+	void insertAtTail(T val);
+	void insertAtIndex(unsigned int index, T val);
 
-		auto size = 1;
-		auto temp = head;
-		while (temp->next) {
-			size++;
-			temp = temp->next;
-		}
-
-		return size;
-	}
-
-
-	void display() const {
-		if (head == nullptr) {
-			std::cout << "List is empty" << '\n';
-			return;
-		}
-
-		auto temp = head;
-
-		while (temp) {
-			std::cout << temp->data << " -> ";
-			temp = temp->next;
-		}
-		std::cout << "null\n";
-	}
-
-
-	void insertAtHead(T val) {
-		if (head == nullptr) {
-			head = std::make_shared<Node<T>>(val);
-			return;
-		}
-		auto tempHead = std::make_shared<Node<T>>(val);
-		tempHead->next = head;
-		head = tempHead;
-	}
-
-	void insertAtTail(T val) {
-		if (head == nullptr) {
-			insertAtHead(val);
-			return;
-		}
-
-		auto temp = head;
-
-		while (temp->next) {
-			temp = temp->next;
-		}
-
-		temp->next = std::make_shared<Node<T>>(val);
-	}
-
-	void insertAtIndex(unsigned int index, T val) {
-		if (index == 0) {
-			insertAtHead(val);
-			return;
-		}
-
-		if (index > length() - 1) {
-			std::cout << "Index larger than size" << '\n';
-			return;
-		}
-
-		auto temp = head;
-		while (index - 1 > 0) {
-			temp = temp->next;
-			index--;
-		}
-
-		auto newNode = std::make_shared<Node<T>>(val);
-		newNode->next = temp->next;
-		temp->next = newNode;
-	}
-
-
-	void deleteAtHead() {
-		if (!head) {
-			std::cout << "List is empty" << '\n';
-			return;
-		}
-
-		head = head->next;
-	}
-
-	void deleteAtTail() {
-		if (!head) {
-			return;
-		}
-
-		auto curr = head;
-		auto prev = curr;
-
-		while (curr->next) {
-			prev = curr;
-			curr = curr->next;
-		}
-
-		prev->next = nullptr;
-	}
-
-	void deleteAtIndex(int index) {
-		if (!head) {
-			return;
-		}
-
-		if (index > length() - 1) {
-			std::cout << "Index larger than size" << '\n';
-			return;
-		}
-
-		auto temp = head;
-		while (index - 1 > 0) {
-			temp = temp->next;
-			index--;
-		}
-
-		temp->next = temp->next->next;
-	}
+	void deleteAtHead();
+	void deleteAtTail();
+	void deleteAtIndex(unsigned int index);
 
 private:
 	std::shared_ptr<Node<T>> head = { nullptr };
@@ -163,6 +49,133 @@ private:
 
 
 
+template<typename T>
+unsigned int LinkedList<T>::length() const {
+	if (!head) {
+		return 0;
+	}
 
+	auto size = 1;
+	auto temp = head;
+	while (temp->next) {
+		size++;
+		temp = temp->next;
+	}
+
+	return size;
+}
+
+template<typename T>
+void LinkedList<T>::display() const {
+	if (head == nullptr) {
+		std::cout << "List is empty" << '\n';
+		return;
+	}
+
+	auto temp = head;
+
+	while (temp) {
+		std::cout << temp->data << " -> ";
+		temp = temp->next;
+	}
+	std::cout << "null\n";
+}
+
+template<typename T>
+void LinkedList<T>::insertAtHead(T val) {
+	if (head == nullptr) {
+		head = std::make_shared<Node<T>>(val);
+		return;
+	}
+	auto tempHead = std::make_shared<Node<T>>(val);
+	tempHead->next = head;
+	head = tempHead;
+}
+
+template<typename T>
+void LinkedList<T>::insertAtTail(T val) {
+	if (head == nullptr) {
+		insertAtHead(val);
+		return;
+	}
+
+	auto temp = head;
+
+	while (temp->next) {
+		temp = temp->next;
+	}
+
+	temp->next = std::make_shared<Node<T>>(val);
+}
+
+template<typename T>
+void LinkedList<T>::insertAtIndex(unsigned int index, T val) {
+	if (index == 0) {
+		insertAtHead(val);
+		return;
+	}
+
+	if (index > length() - 1) {
+		std::cout << "Index larger than size" << '\n';
+		return;
+	}
+
+	auto temp = head;
+	while (index - 1 > 0) {
+		temp = temp->next;
+		index--;
+	}
+
+	auto newNode = std::make_shared<Node<T>>(val);
+	newNode->next = temp->next;
+	temp->next = newNode;
+}
+
+template<typename T>
+void LinkedList<T>::deleteAtHead() {
+	if (!head) {
+		std::cout << "List is empty" << '\n';
+		return;
+	}
+
+	head = head->next;
+}
+
+template<typename T>
+void LinkedList<T>::deleteAtTail() {
+	if (!head) {
+		return;
+	}
+
+	auto curr = head;
+	auto prev = curr;
+
+	while (curr->next) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	prev->next = nullptr;
+}
+
+template<typename T>
+void LinkedList<T>::deleteAtIndex(unsigned int index) {
+	if (!head) {
+		return;
+	}
+
+	if (index > length() - 1) {
+		std::cout << "Index larger than size" << '\n';
+		return;
+	}
+
+	auto temp = head;
+	while (index - 1 > 0) {
+		temp = temp->next;
+		index--;
+	}
+
+	temp->next = temp->next->next;
+}
 
 #endif 
